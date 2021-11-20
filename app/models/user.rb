74611b8ff4b7b7ece7878c4fbcebe546c9e5684d
Gordonamaka <amobimak@gmail.com>
2021-11-20 01:08:03 +0000

    Capybare/Poltergeist Setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,25 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  validates :name, presence:true
-  validates :email, presence:true
-  validates :password, presence:true
+  validates :password, presence: true, length: {minimum: 5}
+  validates :password_confirmation, presence: true, length: {minimum: 5}
+  validates :email, presence: true,  uniqueness: { case_sensitive: false }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 
-  def self.authenticate_with_credentials(email, password)
+  def self.authenticate_with_credentials(email_address, password)
 
-    user = User.find_by_email(email)
-    user && user.authenticate(password)
+    user = User.find_by_email(email_address.strip.downcase)
+
+    if user && user.authenticate(password)
+      return user
+    else
+      nil
+    end
 
   end
 
 end
 
 # Password needs a minimum length, get rid of spaces within registration and login in the email
-#check for uppercases, it should still be logged in
-# If there's time use a prompt for wrong emails
+# Check for uppercases, it should still be logged in
